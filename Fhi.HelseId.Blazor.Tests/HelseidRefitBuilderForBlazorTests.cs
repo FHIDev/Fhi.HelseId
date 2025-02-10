45071cd302d2f723f384596102997667bb6a1a8e
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2025-02-10 16:09:35 +0100

    Converted to encoding UTF-8-BOM

--- Fhi.HelseId.Blazor.Tests/HelseidRefitBuilderForBlazorTests.cs
+++ Fhi.HelseId.Blazor.Tests/HelseidRefitBuilderForBlazorTests.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using Fhi.HelseId.Common.Constants;
 using Fhi.HelseId.Web;
 using Fhi.HelseId.Web.Infrastructure.AutomaticTokenManagement;
